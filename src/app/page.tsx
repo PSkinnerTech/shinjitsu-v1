e92b8047adf61492850b0ebe67d4849006c44b25
Patrick Skinner <78289253+PSkinnerTech@users.noreply.github.com>
2023-10-02 16:32:53 -0500

    Testing functionality to get Identities by Creator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,44 @@
 import Header from '@/components/Header'
-import { getAuthHeaders } from '@/lib/api'
-import { requireAuth } from '@/lib/session'
+import IdentityForm from './identity-form'
 
 export async function getIdentities() {
-  const { didSession, apikey } = await requireAuth()
-  const headers = getAuthHeaders(didSession, apikey)
+  const apiKey = process.env.NEXT_PUBLIC_X_API_KEY
+  const didSession = process.env.NEXT_PUBLIC_DID_SESSION
+  const creatorAddress = process.env.NEXT_PUBLIC_MY_WALLET_ADDRESS
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/query/identities`,
-    {
-      method: 'POST',
-      headers,
-      body: JSON.stringify({}),
+  const headers = {
+    'Content-Type': 'application/json',
+    authorization: `Bearer ${didSession}`,
+    ...(apiKey ? { 'x-api-key': apiKey } : {}),
+  }
+
+  const body = {
+    input: {
+      creator: { value: creatorAddress, op: '=' },
     },
-  )
+  }
+
+  // Logging the request details
+  console.log('URL:', `http://api.intuition.cafe/query/identities`)
+  console.log('Headers:', headers)
+  console.log('Body:', JSON.stringify(body))
+
+  const res = await fetch(`http://api.intuition.cafe/query/identities`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(body),
+  })
   const { data: identities } = await res.json()
-  return { identities }
+  return identities
 }
 
 export default async function Home() {
-  // const identities = await getIdentities()
-  // console.log(identities)
+  const identities = await getIdentities()
+  console.log(identities)
 
   return (
     <>
-      <main
-        className="flex min-h-screen flex-col items-center p-24 gap-y-12"
-        style={{ backgroundColor: '#111315' }}
-      >
+      <main className="flex min-h-screen flex-col items-center p-24 gap-y-12">
         <Header />
         <div className="h-full flex flex-col items-center pt-40">
           <p className="rounded-md px-4 py-3 font-mono text-md cursor-default border border-stone-800/50 bg-gray-50/5 backdrop-blur-sm">
@@ -35,6 +46,7 @@ export default async function Home() {
             <span className="font-bold text-success-500">src/app/page.tsx</span>
           </p>
         </div>
+        <IdentityForm />
       </main>
     </>
   )
